a65bd5315d9481826b13f88ef5847f8e0ece310b
Dinesh Borude <borudedinesh2@gmail.com>
2025-01-19 23:53:53 +0530

    pages initialization

---
+++ src/main/java/PageObjects/web/LoginPage.java
@@ -0,0 +1,16 @@
+package PageObjects.web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    By username = By.id("user-name");
+    By password = By.id("password");
+    By login_button = By.id("login-button");
+
+
+
+}
